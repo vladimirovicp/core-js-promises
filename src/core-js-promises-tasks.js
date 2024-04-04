c00802445edefd4a4769ff8e0df14ea511eda00c
vladimirovicp <vladimirovicp70@gmail.com>
2024-04-04 23:09:40 +0400

    pending 4

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -56,8 +56,20 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise
+        .then((value) => {
+          resolve(value);
+        })
+        .catch(() => {
+          if (promises.every((p) => p.status === 'rejected')) {
+            reject(new Error('All promises are rejected'));
+          }
+        });
+    });
+  });
 }
 
 /**
